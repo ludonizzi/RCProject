d5fb446fd3999087638ca57107abe3f61bfa09b7
DanieleIacomini <danieleiacomini96@gmail.com>
2019-09-14 13:13:19 +0200

    Facebook login

---
+++ myapp/configuration/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  /*
+   * This file contains the configurations information of Twitter login app.
+   * It consists of Twitter app information, database information.
+   */
+
+  facebook_api_key: "396507620905530",
+  facebook_api_secret: "ab708980377ab4dcdb3a764470ddc1e6",
+  callback_url: "http://localhost:3000/auth/facebook/callback",
+  use_database: false,
+  host: "localhost",
+  username: "root",
+  password: "",
+  database: ""
+};
--- myapp/server.js
+++ myapp/server.js
@@ -2,8 +2,25 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+const passport        =     require('passport')
+  , FacebookStrategy  =     require('passport-facebook').Strategy
+  , session           =     require('express-session')
+  , cookieParser      =     require('cookie-parser')
+  , config            =     require('./configuration/config')
+  , mysql             =     require('mysql')
+
+//Define MySQL parameter in Config.js file.
+const pool = mysql.createPool({
+  host     : config.host,
+  user     : config.username,
+  password : config.password,
+  database : config.database
+});
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.set('view engine', 'jade');
 app.use(express.static('public')); //abbiamo istruito express per andare nella dir public ed inserire file statici
@@ -18,6 +35,10 @@ app.get('/home', function(req,res) {
 
 app.get('/contatti', function(req,res) {
     res.redirect('contatti.html');
+})
+
+app.get('/login', function(req,res) {
+    res.redirect('loginFB.html');
 })
 
 
@@ -38,6 +59,85 @@ app.post('/new-user', (req,res) => {
                     }
     res.render('user-confirm', message);
 })
+
+
+
+// Passport session setup.
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+
+// Use the FacebookStrategy within Passport.
+
+passport.use(new FacebookStrategy({
+    clientID: config.facebook_api_key,
+    clientSecret:config.facebook_api_secret ,
+    callbackURL: config.callback_url
+  },
+  function(accessToken, refreshToken, profile, done) {
+    process.nextTick(function () {
+      //Check whether the User exists or not using profile.id
+      if(config.use_database) {
+        // if sets to true
+        pool.query("SELECT * from user_info where user_id="+profile.id, (err,rows) => {
+          if(err) throw err;
+          if(rows && rows.length === 0) {
+              console.log("There is no such user, adding now");
+              pool.query("INSERT into user_info(user_id,user_name) VALUES('"+profile.id+"','"+profile.username+"')");
+          } else {
+              console.log("User already exists in database");
+          }
+        });
+      }
+      return done(null, profile);
+    });
+  }
+));
+
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({ secret: 'keyboard cat', key: 'sid'}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+  res.render('index', { user: req.user });
+});
+
+app.get('/account', ensureAuthenticated, function(req, res){
+  res.render('account', { user: req.user });
+});
+
+app.get('/auth/facebook', passport.authenticate('facebook',{scope:'email'}));
+
+
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { successRedirect : '/', failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login')
+}
+
+
 
 app.listen(3000, function(){
     console.log("Server is listening on port 3000");
