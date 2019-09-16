6c56245d0e6c38b2a5bccc6d7ee6ec8c7893daa6
Riccardo Di Biasi <dibiasi.1694305@studenti.uniroma1.it>
2019-09-16 23:41:07 +0200

    Informazioni sensibili sulle API facebook rimosse

--- myapp/configuration/auth.js
+++ myapp/configuration/auth.js
@@ -1,7 +1,7 @@
 module.exports = {
 	'facebookAuth' : {
-		clientID: '396507620905530',
-		clientSecret: 'ab708980377ab4dcdb3a764470ddc1e6',
+		clientID: process.env.FB_ID,
+		clientSecret: process.env.FB_SECRET,
 		callbackURL: 'http://localhost:3000/auth/facebook/callback',
 		profileURL: 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
 		//profileFields: ['id', 'displayName', 'email', 'first_name', 'middle_name', 'last_name']
